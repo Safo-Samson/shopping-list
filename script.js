3e27bd53c64558f2b1844a717a5363d0b29c908b
Safo Samson <safosamson18@gmail.com>
2023-07-07 12:12:23 +0100

    adding item functionality to shop

--- script.js
+++ script.js
@@ -2,26 +2,42 @@ const itemForm = document.getElementById('item-form');
 const itemInput = document.getElementById('item-input');
 const itemList = document.getElementById('item-list');
 
+function addItem(e) {
+  e.preventDefault();
 
+  const newItem = itemInput.value;
 
+  // Validate Input
+  if (newItem === '') {
+    alert('Please add an item');
+    return;
+  }
 
-function addItem(event){
-    event.preventDefault();
+  // Create list item
+  const li = document.createElement('li');
+  li.appendChild(document.createTextNode(newItem));
 
-    // user entered input
-    let newItem = itemInput.value;
+  const button = createButton('remove-item btn-link text-red');
+  li.appendChild(button);
 
-    if(newItem ===""){
-        alert("Enter something");
-        return;
-    }
+  itemList.appendChild(li);
 
-
-
-
+  itemInput.value = '';
 }
 
+function createButton(classes) {
+  const button = document.createElement('button');
+  button.className = classes;
+  const icon = createIcon('fa-solid fa-xmark');
+  button.appendChild(icon);
+  return button;
+}
 
+function createIcon(classes) {
+  const icon = document.createElement('i');
+  icon.className = classes;
+  return icon;
+}
 
-//event listeners
+// Event Listeners
 itemForm.addEventListener('submit', addItem);
